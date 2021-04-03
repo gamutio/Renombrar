e0b4dcbbac8fd59e612813ff343752351959c1b0
Guille <Guille@DESKTOP-20UAI81>
2021-04-03 18:42:58 +0200

    Aplicación para copiar y renombrar ficheros de una carpeta a otra v0

--- Renombrar/src/Renombrar/Renombrar.java
+++ Renombrar/src/Renombrar/Renombrar.java
@@ -17,7 +17,7 @@ public class Renombrar {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        crearVentana v = new crearVentana();        
+        crearVentana v = new crearVentana();
     }
     
 }
